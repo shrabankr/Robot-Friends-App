bb46c7bea3187e0a17649b92891c4add0236eb18
Shraban Kumar Mahato <shrabankumarmahato@gmail.com>
2019-05-02 02:29:17 +0530

    Web
    File uploaded to the website

---
+++ src/Robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Hinze Ohm',
+        username: 'Hinze',
+        email: 'hohm@nymag.com',
+    },
+    {
+        id: 2,
+        name: 'Denys Bleue',
+        username: 'Denys',
+        email: 'dbleue@prweb.com',
+    },
+    {
+        id: 3,
+        name: 'Elissa Beeken',
+        username: 'Elissa',
+        email: 'ebeeken@plala.or.jp',
+    },
+    {
+        id: 4,
+        name: 'O Tierney',
+        username: 'Tierney',
+        email: 'botierney@earthlink.net',
+    },
+    {
+        id: 5,
+        name: 'Karleen Pollitt',
+        username: 'Karleen',
+        email: 'kpollitt@cpanel.net',
+    },
+    {
+        id: 6,
+        name: 'Leo Hrinchenko',
+        username: 'Leo',
+        email: 'lhrinchenko@wiley.com',
+    },
+    {
+        id: 7,
+        name: 'Valentin Rolles',
+        username: 'Valentin',
+        email: 'vrolles@360.cn',
+    },
+    {
+        id: 8,
+        name: 'Zacherie Hardan',
+        username: 'Zacherie',
+        email: 'zhardan@google.com.au',
+    },
+    {
+        id: 9,
+        name: 'Aditya Singh',
+        username: 'Aditya',
+        email: 'aditya_singh@example.com',
+    },
+    {
+        id: 10,
+        name: 'Tejas Rodrigues',
+        username: 'Tejas',
+        email: 'tejas-85@example.com',
+    },    
+    {
+        id: 11,
+        name: 'Paridhi Pawar',
+        username: 'Paridhi',
+        email: 'paridhi83@mail.com',
+    },
+    {
+        id: 12,
+        name: 'Amit Jaiteley',
+        username: 'Amit',
+        email: 'amit65@ymail.com',
+    }    
+];
